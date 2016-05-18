7b709c92f010901d44fc6f4b463070c4796091ec
PoroshkinaVV <lerik-erik@pochta.ru>
2016-05-18 13:40:06 +0300

    change validation

--- app/models/ticketprice.rb
+++ app/models/ticketprice.rb
@@ -1,10 +1,13 @@
 class Ticketprice < ActiveRecord::Base
   has_many :ticketprice_collections
 
-  validates :visitor_type, presence: true, inclusion: {in: [0, 1, 2, 
-3, 4]}
-  validates :price, numericality: {greater_than: 0}
   
   VISITOR_TYPES = {0=>"взрослый", 1=>"детский", 2=>"иностранный 
 гражданин", 3=>"студент", 4=>"инвалид" }
+
+  validates :price, numericality: {greater_than: 0}
+  validates :visitor_type, presence: true, inclusion: {in: 
+VISITOR_TYPES.keys}
+
+
 end
