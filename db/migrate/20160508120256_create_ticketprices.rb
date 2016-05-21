9320498dae38ca5cb6478fefdb388bbbadd78dfd
PoroshkinaVV <lerik-erik@pochta.ru>
2016-05-21 12:15:53 +0300

    changing field

--- db/migrate/20160508120256_create_ticketprices.rb
+++ db/migrate/20160508120256_create_ticketprices.rb
@@ -1,7 +1,7 @@
 class CreateTicketprices < ActiveRecord::Migration
   def change
     create_table :ticketprices do |t|
-      t.string :visitor_type, null: false, default: 'взрослый'
+      t.integer :visitor_type, null: false, default: 0
       t.float :price, null: false, default: 0
 
       t.timestamps null: false
